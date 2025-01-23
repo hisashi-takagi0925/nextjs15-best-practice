134ec893cb46ced9ab1d13e977276330656d1b37
高木悠 <takagi_hisashi@takagiyuunoMacBook-Air.local>
2025-01-23 11:42:09 +0900

    submitボタン追加

---
+++ src/app/(authorized)/posts/edit/[id]/_components/PageContent/SubmitButton/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/shared/components/client/Button";
+
+export const SubmitButton = () => {
+  return (
+    <Button type="submit" className="min-w-24 font-bold">
+      更新
+    </Button>
+  );
+};
